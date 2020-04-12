16aaad1df34e2759d4755fa737bd0e87331c0210
Curtis Li <cuhtis@fb.com>
2020-04-12 16:32:28 -0400

    Add graphql library

--- src/main/java/com/cuhtis/coup/CoupApplication.java
+++ src/main/java/com/cuhtis/coup/CoupApplication.java
@@ -1,7 +1,11 @@
 package com.cuhtis.coup;
 
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoupApplication {
@@ -10,4 +14,12 @@ public class CoupApplication {
 		SpringApplication.run(CoupApplication.class, args);
 	}
 
+    @Bean
+    GraphQLSchema schema() {
+        return GraphQLSchema.newSchema()
+            .query(GraphQLObjectType.newObject()
+                .name("Query")
+                .build())
+            .build();
+    }
 }
