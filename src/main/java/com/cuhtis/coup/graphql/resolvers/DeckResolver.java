c7553e6517086fb8cfb755bd03eaa9666e333d62
Curtis Li <cuhtis@fb.com>
2020-04-19 17:57:22 -0400

    Expose deck cards instead of just card count

--- src/main/java/com/cuhtis/coup/graphql/resolvers/DeckResolver.java
+++ src/main/java/com/cuhtis/coup/graphql/resolvers/DeckResolver.java
@@ -5,9 +5,4 @@ import graphql.kickstart.tools.GraphQLResolver;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DeckResolver implements GraphQLResolver<IDeck> {
-
-    public int card_count(IDeck deck) {
-        return deck.getCardCount();
-    }
-}
+public class DeckResolver implements GraphQLResolver<IDeck> {}
