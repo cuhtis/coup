e5507304c840518bd90f0e59d7a8041f009ecbce
Curtis Li <cuhtis@fb.com>
2020-04-20 01:04:53 -0400

    Load game from table with standard constructor

--- src/main/java/com/cuhtis/coup/models/game/StandardGame.java
+++ src/main/java/com/cuhtis/coup/models/game/StandardGame.java
@@ -16,8 +16,12 @@ import javax.persistence.Transient;
 @Table(name = "standard_games")
 public class StandardGame extends BaseGame {
 
-    public StandardGame(int numPlayers) {
+    public StandardGame() {
         super(new StandardDeck());
+    }
+
+    public StandardGame(int numPlayers) {
+        this();
         for (int i = 0; i < numPlayers; i++) {
             this.mPlayers.add(new StandardPlayer(this.mDeck));
         }
